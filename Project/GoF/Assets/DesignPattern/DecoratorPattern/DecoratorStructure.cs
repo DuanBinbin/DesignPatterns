da0d328d581432cf0f65f51be976afea5904311f
DuanBinbin <binbin.duan@pisofttech.com>
2018-08-17 18:21:36 +0800

    修改装饰模式的例子

--- Project/GoF/Assets/DesignPattern/DecoratorPattern/DecoratorStructure.cs
+++ Project/GoF/Assets/DesignPattern/DecoratorPattern/DecoratorStructure.cs
@@ -6,6 +6,7 @@
 * Description:		
 * ==============================
 */
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -15,13 +16,73 @@ namespace DecoratorStructure
 	public class DecoratorStructure : MonoBehaviour {
 	
 		void Start () {
-			
-		}
-	
-		void Update () {
-			
+            // Create ConcreteComponent and two Decorators
+            ConcreteComponent component = new ConcreteComponent();
+            ConcreteDecoratorA decoratorA = new ConcreteDecoratorA();
+            ConcreteDecoratorB decoratorB = new ConcreteDecoratorB();
+
+            // Link decorators
+            decoratorA.SetComponent(component);
+            decoratorB.SetComponent(component);
+
+            decoratorB.Operation();
 		}
 	}
+
+    public abstract class Component
+    {
+        public abstract void Operation();
+    }
+
+    public class ConcreteComponent : Component
+    {
+        public override void Operation()
+        {
+            Debug.Log("ConcreteComponent.Operation()");
+        }
+    }
+
+    public abstract class Decorator : Component
+    {
+        protected Component m_component;
+
+        public void SetComponent(Component component)
+        {
+            m_component = component;
+        }
+
+        public override void Operation()
+        {
+            if (m_component != null)
+            {
+                m_component.Operation();
+            }
+        }
+    }
+
+    public class ConcreteDecoratorA : Decorator
+    {
+        public override void Operation()
+        {
+            base.Operation();
+            Debug.Log("ConcreteDecoratorA.Operation()");
+        }
+    }
+
+    public class ConcreteDecoratorB : Decorator
+    {
+        public override void Operation()
+        {
+            base.Operation();
+            AddBehavior();
+            Debug.Log("ConcreteDecoratorB.Operation()");
+        }
+
+        private void AddBehavior()
+        {
+            Debug.Log("ConcreteDecoratorB.AddBehavior()");
+        }
+    }
 }
 
 
